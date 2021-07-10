082ead615c21ac3c8c940420d504ef1d7e39d536
anshika235 <anshika.kulshrestha_da18@gla.ac.in>
2021-07-10 10:02:06 +0530

    PrintDecreasing

---
+++ src/PrintDecreasing.java
@@ -0,0 +1,14 @@
+public class PrintDecreasing {
+    private static void printDec(int n) {
+        if(n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printDec(n - 1);
+
+    }
+    public static void main(String[] args) {
+        printDec(5);
+    }
+
+}
